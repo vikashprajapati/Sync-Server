55cbfdd1ed76ef2b58ab7c7ddb6e0363d2e3f4aa
vikash <prajapativikash0000@gmail.com>
2021-10-19 22:18:23 +0530

    added room join handling for multiple clients

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Console } = require('console')
 const express = require('express')
 const app = express()
 const http = require('http')
@@ -7,24 +8,66 @@ const io = new Server(server)
 
 // constants
 const PORT = 5000
-const THEATER_ROOM = "Theater 1"
 const connections = []
 const users = []
+const rooms = []
 const userRooms = {}
 
 // socket events
 io.on("connection", (socket) => {
     console.log(`connection opened by ${socket.id}`);
     connections.push(socket.id)
-    
-    // initially getting the already created room info
-    socket.emit("joinRoomInfo")
-    socket.on("roomJoinInfo", (roomInfo) => {
-        socket.join(roomInfo)
-        userRooms[socket.id] = roomInfo
-        console.log(`room ${roomInfo} joined`)
+    socket.on("join room", (room, user) => {
         
-        // send information of all participants within this room to newly joined user
+        
+        console.log(io.sockets.adapter.rooms.has(room.name));
+        if(!io.sockets.adapter.rooms.has(room.name)){
+            // create a new room, set the room host & add participants
+            let newRoom = {
+                name : room.name,
+                host : socket.id,
+                participants : [
+                    {
+                        id : socket.id,
+                        name : user.name
+                    }
+                ]
+            }
+            rooms.push(newRoom)
+            console.log(`${room.name} room is created`);
+            
+            socket.emit("joined room response", {
+                room : newRoom
+            })
+            console.log(`joining user in new room ${room.name}, participants count ${newRoom.participants.length}`);
+            
+        }else{
+            // update participant list in room
+            let index = 0
+            for (; index < rooms.length; index++) {
+                if(rooms[index].name == room.name){
+                    rooms[index].participants.push({
+                        id : socket.id,
+                        name : user.name
+                    })
+                    
+                    socket.emit("joined room response", {
+                        room : room[index]
+                    })
+                    console.log(`joining user in existing room ${room.name}, participants count ${rooms[index].participants.length}`);
+                    break;
+                }
+            }
+        }
+        
+        // join the room 
+        socket.join(room.name)
+        console.log(`${user.name} joined room ${room.name}`)
+        userRooms[socket.id] = room.name
+        users.push({
+            id : socket.id,
+            name : user.name
+        })
     }).on("onMessage", (anotherSocket, message) => {
         console.log(`message received from client ${anotherSocket}`)
         console.log(message)
@@ -40,11 +83,18 @@ io.on("connection", (socket) => {
         socket.broadcast.emit("previousVideo")
     }).on("disconnect", (clientId) => {
         console.log(`client ${clientId} disconnected`)
-        socket.to(room).emit("client disconnected", {
-            clientId : clientId
-        })
+        // socket.to(room).emit("client disconnected", {
+        //     clientId : clientId
+        // })
     })
 })
+
+function doesRoomExist(roomInfo) {
+    for (const room in rooms) {
+        if(roomInfo.name == room.name) return true
+    }
+    return false
+}
 
 app.get('/', (req, res) => {
     res.send('<h1>Server is running</h1>')
