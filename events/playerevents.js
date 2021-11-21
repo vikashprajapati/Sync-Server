9125d0a198a59f37088773570c2b6c3e38bcdb02
vikash <prajapativikash0000@gmail.com>
2021-11-21 21:51:06 +0530

    player events payload updated
    added sender id in the payload to tell all the receivers who is the event sender

--- events/playerevents.js
+++ events/playerevents.js
@@ -5,23 +5,32 @@ module.exports = (io) => {
     
     const videoPlayback = function(playbackStatus){
         const socket = this
-        
-        console.log(success(`${playbackStatus} at ${userRooms[socket.id]}`))
-        io.to(userRooms[socket.id]).emit("video playback", playbackStatus)
+        let payload = {
+            id : socket.id,
+            playbackStatus: playbackStatus
+        }
+        console.log(success(`${playbackStatus} at room ${userRooms[socket.id]}`))
+        io.to(userRooms[socket.id]).emit("video playback", JSON.stringify(payload))
     }
     
     const videoChanged = function(playbackDirection){
         const socket = this
-        
-        console.log(success(`${playbackDirection} at ${userRooms[socket.id]}`))
-        io.to(userRooms[socket.id]).emit("video changed", playbackDirection)
+        let payload = {
+            id : socket.id,
+            playbackDirection: playbackDirection
+        }
+        console.log(success(`${playbackDirection} at room ${userRooms[socket.id]}`))
+        io.to(userRooms[socket.id]).emit("video changed", JSON.stringify(payload))
     }
     
     const videoSynced = function(playbackTimestamp){
         const socket = this
-        
+        let payload = {
+            id : socket.id,
+            playbackTimestamp: playbackTimestamp
+        }
         console.log(success(`video synced at ${userRooms[socket.id]}`))
-        io.to(userRooms[socket.id]).emit("video synced", playbackTimestamp)
+        io.to(userRooms[socket.id]).emit("video synced", JSON.stringify(payload))
     }
 
     return {
