f07d8367bd76dbe4d5468a4c25295f5375b4210e
vikash <prajapativikash0000@gmail.com>
2021-11-27 23:41:12 +0530

    updated videoChanged event payload

--- events/playerevents.js
+++ events/playerevents.js
@@ -6,27 +6,23 @@ module.exports = (io) => {
     const videoPlayback = function(playbackStatus){
         const socket = this
         let payload = {
-            id : socket.id,
+            userId : socket.id,
             playbackStatus: playbackStatus
         }
         console.log(success(`${playbackStatus} at room ${userRooms[socket.id]}`))
         io.to(userRooms[socket.id]).emit("video playback", JSON.stringify(payload))
     }
     
-    const videoChanged = function(playbackDirection){
+    const videoChanged = function(payload){
         const socket = this
-        let payload = {
-            id : socket.id,
-            playbackDirection: playbackDirection
-        }
-        console.log(success(`${playbackDirection} at room ${userRooms[socket.id]}`))
-        io.to(userRooms[socket.id]).emit("video changed", JSON.stringify(payload))
+        console.log(success(`${payload} at room ${userRooms[socket.id]}`))
+        io.to(userRooms[socket.id]).emit("video changed", payload)
     }
     
     const videoSynced = function(playbackTimestamp){
         const socket = this
         let payload = {
-            id : socket.id,
+            userId : socket.id,
             playbackTimestamp: playbackTimestamp
         }
         console.log(success(`video synced at ${userRooms[socket.id]}`))
