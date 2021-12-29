819e4baa9337dd158a47f9deb28bb430d520dbba
vikash <prajapativikash0000@gmail.com>
2021-12-29 23:05:38 +0530

    added ping event & pong emitter

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
-const { Console } = require('console')
-const express = require('express')
-const app = express()
-const http = require('http')
-const { Server } = require('socket.io')
-const server = http.createServer(app)
-const io = new Server(server)
+const { Console } = require("console");
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
 
 // user modules imports
-const { videoPlayback, videoChanged, videoSynced, newVideoSelected } = require('./events/playerevents')(io)
-const { joinRoom, userLeft } = require('./events/roomEvents')(io)
-const { onMessage } = require('./events/chatEvents')(io)
-const { connections } = require('./store')
-const { highlight } = require('./logger')
+const { videoPlayback, videoChanged, videoSynced, newVideoSelected } =
+  require("./events/playerevents")(io);
+const { joinRoom, userLeft } = require("./events/roomEvents")(io);
+const { onMessage } = require("./events/chatEvents")(io);
+const { connections } = require("./store");
+const { highlight } = require("./logger");
 
 // constants
-const PORT = 5000
-app.set('port', (process.env.PORT || PORT));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
- });
+const PORT = 5000;
+app.set("port", process.env.PORT || PORT);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 // socket connection callbacks
 const onConnection = (socket) => {
-    console.log(highlight(`connection opened by ${socket.id}`));
-    connections.push(socket.id)
+  console.log(highlight(`connection opened by ${socket.id}`));
+  connections.push(socket.id);
 
-    socket.on("join room", joinRoom)
+  socket.on("ping", function () {
+    socket.emit("pong");
+  });
 
-    socket.on("on message", onMessage)
+  socket.on("join room", joinRoom);
 
-    socket.on("leave room", userLeft)
+  socket.on("on message", onMessage);
 
-    socket.on("video playback", videoPlayback)
+  socket.on("leave room", userLeft);
 
-    socket.on("video changed", videoChanged)
+  socket.on("video playback", videoPlayback);
 
-    socket.on("video synced", videoSynced)
+  socket.on("video changed", videoChanged);
 
-    socket.on("new video selected", newVideoSelected)
+  socket.on("video synced", videoSynced);
 
-}
+  socket.on("new video selected", newVideoSelected);
+};
 
 // socket connection
-io.on("connection", onConnection)
+io.on("connection", onConnection);
 
 // server listening
-server.listen(app.get('port'), () => {
-    console.log(highlight(`\nServer started at port ${PORT}\n`))
-})
+server.listen(app.get("port"), () => {
+  console.log(highlight(`\nServer started at port ${PORT}\n`));
+});
